248e970b3f22a636a37bf67f673c165189e88e81
12061988 <m.maguettefall@gmail.com>
2022-07-21 19:38:35 +0000

    Add function to results

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ setInterval(score, 50);
 function convert(word) {
     if(word === "paper") return '<i class="fa-solid fa-hand-paper"></i>';
     if(word === "rock") return '<i class="fa-solid fa-hand-rock"></i>';
-    return '<i class="fa-solid fa-hand-rock"></i>'
+    return '<i class="fa-solid fa-hand-rock"></i>';
 }
 
 function game(UserChoice) {
@@ -20,5 +20,57 @@ function game(UserChoice) {
     var userDiv = document.getElementById("UserObject");
     userDiv.innerHTML = convert(UserChoice);
     var computerDiv = document.getElementById("ComputerObject");
-    
+    computerDiv.innerHTML = convert(ComputerChoice);
+    if(UserChoice === "paper" && ComputerChoice === "rock" || UserChoice === "rock" && ComputerChoice === "scissors" || UserChoice === "scissors" && ComputerChoice === "paper"){  
+               win(UserChoice);
+           } 
+           else if(UserChoice === ComputerChoice) {
+               draw(UserChoice);
+           }
+           else {
+               lose(UserChoice);
+           }
+           function continuGame(){
+            i = Math.floor(Math.random() * 3);
+            ComputerChoice = choices[i];
+            box.style.display = "none";
+           }
+           setTimeout(continuGame, 1200);
+
+}
+
+function win(btn) {
+    UserPoints++;
+    document.getElementById("who").innerHTML = "Well Done! You win!";
+    var btn = document.getElementById("btn");
+    btn.classList.remove("btn");
+    btn.classList.remove("green");
+    setTimeout(() => {
+        btn.classList.add("btn");
+        btn.classList.remove("green");
+    }, 1200);
+}
+
+function draw(btn) {
+    document.getElementById("who").innerHTML = "It's a Draw!";
+    var btn = document.getElementById("btn");
+    btn.classList.remove("btn");
+    btn.classList.remove("gray");
+    setTimeout(() => {
+        btn.classList.add("btn");
+        btn.classList.remove("gray");
+    }, 1200);
+
+}
+
+function lose(btn) {
+document.getElementById("who").innerHTML = "Sorry, You lose...";
+var btn = document.getElementById("btn");
+    btn.classList.remove("btn");
+    btn.classList.remove("red");
+    setTimeout(() => {
+        btn.classList.add("btn");
+        btn.classList.remove("red");
+    }, 1200);
+
 }
